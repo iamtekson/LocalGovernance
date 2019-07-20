c08ab81d2a7568fd8d57e40a9fc8fc542711bc6c
shivashrestha7 <shiv.shres25@gmail.com>
2019-07-20 14:51:55 +0545

    ssss

--- app/src/main/java/com/digitalized_nepal/localgovernance/SplashScreen.java
+++ app/src/main/java/com/digitalized_nepal/localgovernance/SplashScreen.java
@@ -49,7 +49,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.activity_splash_screen);
 
 
         mDelayHandler.postDelayed(runnable, SPLASH_DELAY);

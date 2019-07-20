6f2c6e2c5e090d33870d053aa5aac803ad5f303b
shivashrestha7 <shiv.shres25@gmail.com>
2019-07-20 14:30:48 +0545

    shiv first commit

---
+++ app/src/main/java/com/digitalized_nepal/localgovernance/SplashScreen.java
@@ -0,0 +1,69 @@
+package com.digitalized_nepal.localgovernance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashScreen extends AppCompatActivity {
+    //TextViews
+    private TextView mAppNameView;
+    private TextView mAppVersionView;
+    private ImageView mAppLogoView;
+
+    //Time delays
+    private static final Long SPLASH_DELAY = 2500L;
+
+    private Handler mDelayHandler = new Handler();
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!isFinishing()) {
+                Intent intent;
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    intent = new Intent(SplashScreen.this, UserLoginActivity.class);
+                } else {
+//                    if (Injection.getSharedPreference().getBoolean(SharedPrefConstants.IS_USER_DETAILS_ENTERED)) {
+                    intent = new Intent(SplashScreen.this, HomeActivity.class);
+
+                }
+                finish();
+                overridePendingTransition(R.anim.enter, R.anim.exit);
+
+                startActivity(intent);
+            }
+        }
+    };
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        mDelayHandler.postDelayed(runnable, SPLASH_DELAY);
+
+
+
+    }
+
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDelayHandler.removeCallbacks(runnable);
+    }
+}
